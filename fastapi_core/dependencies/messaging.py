7a66cb26f8acf5f979f7521c3dd36d4ae392e5d3
kdkim <gkd1208@gmail.com>
2026-06-14 15:28:54 +0900

    feat: Remove NATS client implementation and related tests from core and dependencies

--- fastapi_core/dependencies/messaging.py
+++ fastapi_core/dependencies/messaging.py
@@ -6,7 +6,6 @@ from fastapi.params import Depends as DependsParam
 
 from fastapi_core.bootstrap import get_or_create_state_value_async, set_state_value_async
 from fastapi_core.core.config import EnvConfig
-from fastapi_core.core.messaging import create_nats_client
 from fastapi_core.dependencies.config import get_config
 from fastapi_core.docmesh_bridge import get_required_docmesh_service_async
 
